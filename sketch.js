80ab10cf96d07650e8eb2f104926ecf80976b2bd
daavide03 <113475658+daavide03@users.noreply.github.com>
2022-11-23 12:55:59 +0100

    Tagging graffiti drawing app

--- sketch.js
+++ sketch.js
@@ -1,16 +1,110 @@
+var lines = [];
+// let cx;
+// let cy;
+
 function preload() {
-	// put preload code here
+  img = loadImage("./assets/muro.png");
+  imgmask = loadImage("./assets/muromask.png");
 }
 
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	// put setup code here
-	const message = "This is a template reposotory\nfor the course elective Creative Coding\nCommunication Design, Politecnico di Milano";
-	textAlign(CENTER, CENTER);
-  textSize(16)
-	text(message, width/2, height/2);
+  createCanvas(windowWidth, windowHeight);
+  background(img);
+  // cx = width/2;
+  // cy = height/2;
+  colorPicker = createColorPicker('#ed225d');
+  colorPicker.position(5, height -100);
+  frameRate(90);
+
 }
 
 function draw() {
-	// put drawing code here
+
+  col = colorPicker.color();
+  noFill();
+  stroke(col);
+  const dx = constrain(rotationY, -5, 5);
+  const dy = constrain(rotationX, -3, 3);
+
+  if (touchMoved){
+
+    var linea = new mylines();
+    lines.push(linea);
+    
+
+    for (i=0;i<50;i++){
+
+    push();
+    translate(mouseX,mouseY);
+    rotate(random(PI*2));
+    noStroke();
+    fill(col.levels[0]+random(-25,25),col.levels[1]+random(-25,25),col.levels[2]+random(-25,25),random(0,10));
+    beginShape();
+    for (m = 0; m < PI * 2; m += 1) {
+      r = random(2, 10);
+      let x = cos(m) * r;
+      let y = sin(m) * r;
+      vertex(x, y);
+    }
+    endShape(CLOSE);
+    pop();
+  }
+
+  }
+
+  // for (var linea of lines) {
+  //   linea.show();
+  // }
+ 
+  noStroke();
+  image(imgmask,0,0,width,height);
 }
+
+
+
+
+
+
+class mylines {
+  constructor(){
+    this.px=pmouseX;
+    this.py=pmouseY;
+    this.cx=mouseX
+    this.cy=mouseY;
+    this.col=colorPicker.color();
+  }
+  show() {
+    push();
+    strokeWeight(3);
+    stroke(this.col);
+    noFill();
+    blendMode(MULTIPLY);
+    line(this.px,this.py,this.cx,this.cy)
+    pop();
+  }
+}
+
+
+
+
+function touchMoved() {
+  return false;
+}
+
+document.ontouchmove = function(event) {
+  event.preventDefault();
+};
+
+// ask for permissions on iOS
+function touchEnded(event) {
+  // check that those functions exist
+  // if they exist it means we are
+  // on iOS and we can request the permissions
+  if(DeviceOrientationEvent && DeviceOrientationEvent.requestPermission){
+    DeviceOrientationEvent.requestPermission()
+    console.log("AAA");
+  }
+}
+
+
